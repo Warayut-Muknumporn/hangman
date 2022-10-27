f4fff2c915a030dbea982c8015e7f34764738c77
Warayut Muknumporn <warayut.m1@gmail.com>
2022-10-27 03:30:06 +0100

    second time

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Hangman is a classic game in which a player thinks of a word and the other player tries to guess that word within a certain amount of attempts.
 
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it. 
+
+funtion check guess: check a single alphabet and that alphabet is in a word or not
+funtion ask_for_input: ask for guess a single alphabet
--- milestone_3.py
+++ milestone_3.py
@@ -1,21 +1,33 @@
 import random
 from re import L
 
-
-
 list = ['orange','mango','blueburry','papaya','apple']
 word_list = list
 word = random.choices(word_list)
 #print(random.choices(mylist, weights = [10, 1, 1], k = 14))   weight = weigh the possibility , k = integer defining the length of the returned list
 print(word)
 
-guess = input("Enter single letter ")
-while True :
-    if len(guess) == 1 and guess.isalpha():
-        print("Good guess!")
-        break
-    print("Invalid letter. Please, enter a single alphabetical character.")
+
+def check_guess(guess):
+    while True :
+        if len(guess) == 1 and guess.isalpha():
+            print(f"Good guess!: {guess}")
+            guess = guess.lower()
+            break
+        print("Invalid letter. Please, enter a single alphabetical character.")
+        guess = input("Enter single letter ")
+
+    if guess in word[0]:
+        print(f"Good guess! {guess} is in the word")
+    else:
+        print(f'Sorry, {guess} is not in the word. Try again.')
+        
+    return guess
+
+
+def ask_for_input():
     guess = input("Enter single letter ")
 
+    check_guess(guess)
 
-def check_guess():
+ask_for_input()
